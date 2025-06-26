3754e9c69b33c2903fab72b244043a2205d204ca
sunha20 <c.seonha20@gmail.com>
2025-06-26 22:21:42 +0900

    finish

--- rbtree_lab/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /tmp/rbtree_lab
-# Build directory: /tmp/rbtree_lab/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_rbtree "/tmp/rbtree_lab/cmake-build-debug/test_rbtree")
-set_tests_properties(test_rbtree PROPERTIES  _BACKTRACE_TRIPLES "/tmp/rbtree_lab/CMakeLists.txt;53;add_test;/tmp/rbtree_lab/CMakeLists.txt;0;")
